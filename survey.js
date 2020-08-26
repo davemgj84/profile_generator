9f825e56b8067cd6c71607318f611b9684eb962f
davemgj84 <davemgj@gmail.com>
2020-08-26 16:45:50 +0000

    wrapped everything in single function - made modular and reusable

--- survey.js
+++ survey.js
@@ -5,16 +5,19 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question("What's your name? => ", (name) => {
-  rl.question("What's your hero class? (Wizard, Barbarian, Ranger, Thief, Knight, Battle Mage, Scholar, Rogue, Coder) => ", (hero) => {
-    rl.question("Where are you from? => ", (from) => {
-      rl.question("What is your weapon of choice? (wand, axe, bow, dagger, sword, orb, grimoire, crossbow, mechanical keyboard) => ", (weapon) => {
-        rl.question("Do you fight against good or evil => ", (nemesis) => {
-          rl.question("How do you approach a problem or challenge? (boldly, cautiously, sneakily, bravely, creatively, methodically, magically) => ", (approach) => {
-            rl.question("What is your superpower? => ", (power) => {
-              rl.question("If you defeat your enemies, what do you desire most? => ", (desire) => {
-                console.log(`${name} the ${hero}, hails from the far distant lands of ${from}. \nBrandishing a powerful ${weapon}, fighting against the reign of ${nemesis}. \n${name} ${approach} overcomes the powers of ${nemesis} using the immense power of ${power} to triumph. ${name} returns to ${from}, proclaimed a hero to all. \nYou are rewarded with all the ${desire} that you can imagine!`);
-                rl.close();
+const hero = () => {
+
+  rl.question("What's your name? => ", (name) => {
+    rl.question("What's your hero class? (Wizard, Barbarian, Ranger, Thief, Knight, Battle Mage, Scholar, Rogue, Coder) => ", (hero) => {
+      rl.question("Where are you from? => ", (from) => {
+        rl.question("What is your weapon of choice? (wand, axe, bow, dagger, sword, orb, grimoire, crossbow, mechanical keyboard) => ", (weapon) => {
+          rl.question("Do you fight against good or evil => ", (nemesis) => {
+            rl.question("How do you approach a problem or challenge? (boldly, cautiously, sneakily, bravely, creatively, methodically, magically) => ", (approach) => {
+              rl.question("What is your superpower? => ", (power) => {
+                rl.question("If you defeat your enemies, what do you desire most? => ", (desire) => {
+                  console.log(`${name} the ${hero}, hails from the far distant lands of ${from}. \nBrandishing a powerful ${weapon}, fighting against the reign of ${nemesis}. \n${name} ${approach} overcomes the powers of ${nemesis} using the immense power of ${power} to triumph. ${name} returns to ${from}, proclaimed a hero to all. \nYou are rewarded with all the ${desire} that you can imagine!`);
+                  rl.close();
+                });
               });
             });
           });
@@ -22,4 +25,9 @@ rl.question("What's your name? => ", (name) => {
       });
     });
   });
-});
+
+};
+
+hero();
+
+module.exports = hero;
