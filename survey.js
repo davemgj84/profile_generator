b171322257658f96548564d7fed9d1a495be2ebb
davemgj84 <davemgj@gmail.com>
2020-08-26 16:03:27 +0000

    improved version, was too tired to do it last night! Now a superhero profile generator

--- survey.js
+++ survey.js
@@ -5,23 +5,23 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-const profile = [];
+const profile = {};
 
-rl.question("What's your name?", (answer) => {
-  profile.push(answer);
-  rl.question("What's an activity you like doing?", (answer) => {
-    profile.push(answer);
-    rl.question("What do you listen to while doing that?", (answer) => {
-      profile.push(answer);
-      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.)", (answer) => {
-        profile.push(answer);
-        rl.question("What's your favourite thing to eat for that meal?", (answer) => {
-          profile.push(answer);
-          rl.question("Which sport is your absolute favourite?", (answer) => {
-            profile.push(answer);
-            rl.question("What is your superpower? In a few words, tell us what you are amazing at!", (answer) => {
-              profile.push(answer);
-              console.log(profile.join(' '));
+rl.question("What's your name? => ", (answer1) => {
+  profile[1] = answer1;
+  rl.question("What's your hero class? (Wizard, Barbarian, Ranger, Thief, Knight, Battle Mage, Scholar, Rogue, Coder) => ", (answer2) => {
+    profile[2] = answer2;
+    rl.question("Where are you from?", (answer3) => {
+      profile[3] = answer3;
+      rl.question("What is your weapon of choice? (wand, axe, bow, dagger, sword, orb, grimoire, crossbow, mechanical keyboard) => ", (answer4) => {
+        profile[4] = answer4;
+        rl.question("Do you fight against good or evil", (answer5) => {
+          profile[5] = answer5;
+          rl.question("How do you approach a problem or challenge? (boldly, cautiously, sneakily, bravely, creatively, methodically, magically) => ", (answer6) => {
+            profile[6] = answer6;
+            rl.question("What is your superpower? => ", (answer7) => {
+              profile[7] = answer7;
+              console.log(`${profile[1]} the ${profile[2]}, hailsfrom the far distant lands of ${profile[3]}. \nBrandishing a powerful ${profile[4]}, fighting against the reign of ${profile[5]}. \n${profile[1]} ${profile[6]}, overcomes the powers of ${profile[5]} using the immense power of ${profile[7]} to triumph.`);
               rl.close();
             });
           });
